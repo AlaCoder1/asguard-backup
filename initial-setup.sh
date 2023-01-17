76d765c1e420ffe2177afe51b0741b43a891f124
Zied Chebbi <z.chebbi@numeryx.fr>
2023-01-17 08:36:32 +0000

    Update initial-setup.sh

--- initial-setup.sh
+++ initial-setup.sh
@@ -6,7 +6,7 @@ sudo yum -y update
 
 sudo yum install -y epel-release
 
-sudo yum install -y python3 gcc nginx git nano java-1.8.0-openjdk-devel wget
+sudo yum install -y python3.10 gcc nginx git nano java-1.8.0-openjdk-devel wget
 
 sudo systemctl daemon-reload
 

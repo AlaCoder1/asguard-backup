093ebb8a4dd529352cf25f561ae6a8899098a3ad
MH.B <mh.benelgali@numeryx.fr>
2023-07-21 15:19:55 +0000

    Users Groups Servers DATA

--- managementGroup/models.py
+++ managementGroup/models.py
@@ -3,6 +3,7 @@ from django.db import models
 # Create your models here.
 class Group(models.Model):
     groupname = models.CharField(max_length=200, null=True,unique=True)
+    description = models.CharField(max_length=200, null=True)
     gid = models.IntegerField(null=True,unique=True)
     createdBySystem = models.BooleanField(default=False)
     

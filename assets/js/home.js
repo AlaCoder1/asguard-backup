d2ba45bb5faff93b5c47ef6a0fb6a2939caafe91
OO <o.ounifi@numeryx.fr>
2023-08-04 15:23:40 +0000

    add vuex package

--- assets/js/home.js
+++ assets/js/home.js
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import vuetify from '@/plugins/vuetify';
 import App from '@/pages/home';
+import store from './store'; // Adjust the path to your store.js file
 
 // any CSS you import will output into a single css file (app.css in this case)
 
 
 new Vue({
+    store,
     vuetify,
 
     data: {
@@ -15,7 +17,8 @@ new Vue({
         // this.tab= this.$el.attributes['users'] ? this.$el.attributes['users'].value : '';
         this.tab = this.$el.attributes['users'].value;
    
-        console.log("users aa " + JSON.stringify(this.$el.attributes['users'].value));
+        console.log("users object " + JSON.stringify(this.$el.attributes['users'].value));
+
 
     },
     // render: (h) => h(App, { props: { users_data: this.tab } }),

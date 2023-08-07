e81201454fa77ac00358fa1d4afaeed773e09ce6
OO <o.ounifi@numeryx.fr>
2023-08-07 14:02:28 +0000

    ajout i18 et validation

--- assets/js/openvpn.js
+++ assets/js/openvpn.js
@@ -3,8 +3,42 @@ import vuetify from '@/plugins/vuetify';
 import 'vuetify/dist/vuetify.min.css'
 import App from '@/pages/openvpn';
 
+
+import VeeValidate from 'vee-validate';
+import VueI18n from 'vue-i18n';
+import dictionnary from './dictionnary';
+import enJson from './translations/en.json'; 
+import frJson from './translations/fr.json'; 
+
+Vue.use(VueI18n);
+
+const i18n = new VueI18n({
+  locale: 'en',
+  messages: {
+    en: enJson, 
+    fr: frJson, 
+  },
+});
+
+Vue.use(VeeValidate, {
+  i18n,
+  classes: true,
+  fieldsBagName: 'formFields',
+  dictionary: {
+    en: {
+      messages: enJson.messages,
+      attributes: dictionnary,
+    },
+    fr: {
+      messages: frJson.messages,
+      attributes: dictionnary,
+    },
+  },
+});
+
 new Vue({
     vuetify,
+    i18n,
     data: {},
     render: (h) => h(App),
 }).$mount('#app');

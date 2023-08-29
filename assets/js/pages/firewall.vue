fa4690e614d91e742a6c5a02c62708d27ce5bda0
ines.bouguerra <i.bouguerra@numeryx.fr>
2023-08-29 15:18:55 +0000

    Fix tabs

--- assets/js/pages/firewall.vue
+++ assets/js/pages/firewall.vue
@@ -1,8 +1,18 @@
 <template>
   <v-app id="inspire">
-    <base-layout title="Firewall" active-menu="firewall">
+    <base-layout title="Rules" active-menu="firewall">
       <template #content>
-        <FirewallComponent />
+        <v-tabs v-model="activeTab">
+            <v-tab v-for="tab in tabs" :key="tab.id">
+              {{ tab.label }}
+            </v-tab>
+            <v-tab-item v-for="tab in tabs" :key="tab.id">
+              <FirewallComponent v-if="tab.id == 1" activeTab="Flottant" />
+              <FirewallComponent v-if="tab.id == 2" activeTab="LAN" />
+              <FirewallComponent v-if="tab.id == 3" activeTab="Loopback" />
+              <FirewallComponent v-if="tab.id == 4" activeTab="WAN" />
+            </v-tab-item>
+          </v-tabs>
       </template>
     </base-layout>
   </v-app>
@@ -19,6 +29,14 @@ export default {
 },
   data() {
     return {
+       activeTab: 0,
+      tabs: [
+        { id: 1, label: 'Flottant' },
+        { id: 2, label: 'LAN' },
+        { id: 3, label: 'Loopback' },
+        { id: 4, label: 'WAN' },
+      ],
+      firewall: ""
     };
   },
   beforeMount() {

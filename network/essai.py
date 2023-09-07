31d7a5ef4d92c459a50229075d18c16177bd6c22
a.bouhrira <a.bouhrira@numeryx.fr>
2023-09-07 14:43:26 +0200

    correct firewall save all rules

--- network/essai.py
+++ network/essai.py
@@ -17,16 +17,13 @@ def get_conn_name(ifname):
         uuid=output[1]
         return uuid
 ##    
-# uuid=get_conn_name("eth0")
 
 def update_conn_Static_IPV4(uuid,ipaddress,netmask,gwaddr):
+    
     commandes=[
         "sudo nmcli connection modify {} ipv4.method manual ipv4.addresses {}/{}".format(uuid,ipaddress,netmask),
         "sudo nmcli connection modify {} ipv4.gateway {} ipv4.route-metric 0 ".format(uuid,gwaddr),
-        # "sudo  nmcli con reload {}".format(uuid)
-        "sudo nmcli conn down {}".format(uuid),
-        "sudo nmcli conn up {}".format(uuid),
-        # "sudo nmcli connection modify  {} ipv4.addresses {}/{}".format(uuid,ipaddress,netmask),
+         "sudo nmcli conn down {} && sudo nmcli conn up {}".format(uuid, uuid),
     ]
     for cmd in commandes:
         stdin, stdout, stderr = ssh.exec_command('{}'.format(cmd))
@@ -36,9 +33,25 @@ def update_conn_Static_IPV4(uuid,ipaddress,netmask,gwaddr):
             print("error",error,cmd)
         else:
             print("executed successfully!! ",cmd)
-ipaddress="10.1.12.120"
+ipaddress="10.1.12.130"
 netmask=32
-gwaddr="192.1.13.1"
+gwaddr="10.1.13.1"
+mtu=1800
 uuid=get_conn_name("eth3")
 print(uuid)
-update_conn_Static_IPV4(uuid,ipaddress,netmask,gwaddr)        
+# update_conn_Static_IPV4(uuid,ipaddress,netmask,gwaddr)        
+def update_conn_DHCP_IPV4(uuid,ifname):
+    commandes=[
+        "sudo nmcli connection modify {} ipv4.method auto ipv4.addresses '' ipv4.gateway '' ipv4.route-metric '' ".format(uuid),
+        "sudo nmcli conn down {} && sudo nmcli conn up {}".format(uuid, uuid),
+        "sudo dhclient -v -cf  /etc/Dhcp4Config/{}/dhclient.conf".format(ifname),
+    ]
+    for cmd in commandes:
+        stdin, stdout, stderr = ssh.exec_command('{}'.format(cmd))
+        output = stdout.read().decode('utf-8')
+        error = stderr.read().decode('utf-8')
+        if error!="" and not error.startswith("Warning") :
+            print("error",error,cmd)
+        else:
+            print("executed successfully!! ",cmd)
+update_conn_DHCP_IPV4(uuid,"eth3")

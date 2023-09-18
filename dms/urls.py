24be0f8924c4284a017b127c85204e2e00791292
akram baklouti <a.baklouti@numeryx.fr>
2023-09-18 08:22:29 +0000

    fix error in urls

--- dms/urls.py
+++ dms/urls.py
@@ -42,6 +42,6 @@ urlpatterns = [
     path('ipsec/', include('ipsec.urls')),
     path('rules/', include('rules.urls')),
     path('openvpn/', include('openvpn.urls')),
-    path('certificates/', include('managementCertificates.urls'))
-    path('gateway/', include('gateway.urls'))
+    path('certificates/', include('managementCertificates.urls')),
+    path('gateway/', include('gateway.urls')),
 ]

93ee1a106381cde274838b5dd8dbdb63c8f33edd
A.S <s.aloui@numeryx.fr>
2023-10-12 21:49:53 +0100

    Add-Error-UI

--- network/views.py
+++ network/views.py
@@ -241,7 +241,7 @@ def conf(request,name_interface):
                 cmd_asguard="""sudo cat <<EOF > /etc/systemd/system/Asguard-Networking.service
 {}
 EOF""".format('\n'.join(output_service))
-                if run_all_commands(commandes_final,setuptypeIP4,5) is True:
+                if run_all_commands(commandes_final,setuptypeIP4,10) is True:
                     output, error=run_command(cmd_asguard)
                     if  (error==""):
                         if setuptypeIP4.lower()=="dhcp":
@@ -311,7 +311,7 @@ EOF""".format('\n'.join(output_service))
                         msg=error
                         status=400        
                 else:
-                    msg=run_all_commands(commandes_final,setuptypeIP4,5)
+                    msg=run_all_commands(commandes_final,setuptypeIP4,10)
                     status=400
             else:
                 msg="Failed to configure Network Service not found!!"

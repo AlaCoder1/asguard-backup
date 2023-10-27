479928a46ca2180e4a1a2811f45750b6bc335c1b
MH.B <o.ounifi@numeryx.fr>
2023-10-27 16:53:40 +0100

    add payment in menu

--- subscription/urls.py
+++ subscription/urls.py
@@ -3,10 +3,12 @@ from . import views
 
 urlpatterns = [
     path('payment', views.payment, name="payment"),
-    path('add_plan', views.add_plan, name="add_plan"),
-    path('add_organizations', views.add_organizations, name="add_organizations"),
-    path('add_paymentTransaction', views.add_paymentTransaction, name="add_paymentTransaction"),
-    path('add_plansSubscription', views.add_plansSubscription, name="add_plansSubscription"),
-    path('add_plansFeatures', views.add_plansFeatures, name="add_plansFeatures"),
+    # path('add_plan', views.add_plan, name="add_plan"),
+    # path('add_organizations', views.add_organizations, name="add_organizations"),
+    # path('add_paymentTransaction', views.add_paymentTransaction, name="add_paymentTransaction"),
+    # path('add_plansSubscription', views.add_plansSubscription, name="add_plansSubscription"),
+    # path('add_plansFeatures', views.add_plansFeatures, name="add_plansFeatures"),
+    path('check_subscription', views.check_subscription, name="check_subscription"),
+    path('paymentFromMenu', views.paymentFromMenu, name="paymentFromMenu"),
 ]
 

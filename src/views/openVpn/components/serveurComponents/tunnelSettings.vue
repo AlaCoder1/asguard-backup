177d939325b2f5ac4ebc656b72e326ae93ce2104
A.S <s.aloui @numeryx.fr>
2023-11-07 21:30:35 +0100

    OpenVpn API Server

--- src/views/openVpn/components/serveurComponents/tunnelSettings.vue
+++ src/views/openVpn/components/serveurComponents/tunnelSettings.vue
@@ -7,114 +7,212 @@
         <label>IPv4 Tunnel Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv4 Tunnel Network"></v-text-field>
+        <v-text-field
+          label="IPv4 Tunnel Network"
+          v-model="ip4Tunnel"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>IPv6 Tunnel Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv6 Tunnel Network"></v-text-field>
+        <v-text-field
+          label="IPv6 Tunnel Network"
+          v-model="ip6Tunnel"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>Gateway</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+        <input type="checkbox" v-model="isGateway" />
         <label class="ml-2">redirect Gateway</label>
       </v-col>
 
-      <v-col cols="4">
+      <v-col cols="4" v-if="deviceMode === 'tap'">
         <label>Bridge</label>
       </v-col>
-      <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+      <v-col cols="8" class="mb-n6" v-if="deviceMode === 'tap'">
+        <input type="checkbox" v-model="isBridge" />
         <label class="ml-2">DHCP bridge</label>
       </v-col>
 
-      <v-col cols="4">
+      <v-col cols="4" v-if="isBridge">
         <label>Interface bridge</label>
       </v-col>
-      <v-col cols="8" class="mb-n6">
-        <v-select label="Interface bridge"></v-select>
+      <v-col cols="8" class="mb-n6" v-if="isBridge">
+        <v-select label="Interface bridge" v-model="interfaceBridge"></v-select>
       </v-col>
 
-      <v-col cols="4">
+      <v-col cols="4" v-if="isBridge">
         <label>Start DHCP bridge</label>
       </v-col>
-      <v-col cols="8" class="mb-n6">
-        <v-text-field label="Start DHCP bridge"></v-text-field>
+      <v-col cols="8" class="mb-n6" v-if="isBridge">
+        <v-text-field
+          label="Start DHCP bridge"
+          v-model="startDHCPBridge"
+        ></v-text-field>
       </v-col>
-      <v-col cols="4">
+      <v-col cols="4" v-if="isBridge">
         <label>End DHCP bridge</label>
       </v-col>
-      <v-col cols="8" class="mb-n6">
-        <v-text-field label="End DHCP bridge"></v-text-field>
+      <v-col cols="8" class="mb-n6" v-if="isBridge">
+        <v-text-field
+          label="End DHCP bridge"
+          v-model="endDHCPBridge"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>IPv4 Local Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv4 Local Network"></v-text-field>
+        <v-text-field
+          label="IPv4 Local Network"
+          v-model="iPv4Local"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>IPv6 Local Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv6 Local Network"></v-text-field>
+        <v-text-field
+          label="IPv6 Local Network"
+          v-model="iPv6Local"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>IPv4 Remote Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv4 Remote Network"></v-text-field>
+        <v-text-field
+          label="IPv4 Remote Network"
+          v-model="iPv4Remote"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>IPv6 Remote Network</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="IPv6 Remote Network"></v-text-field>
+        <v-text-field
+          label="IPv6 Remote Network"
+          v-model="iPv6Remote"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>Concurrent connections</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-text-field label="Concurrent connections"></v-text-field>
+        <v-text-field
+          label="Concurrent connections"
+          v-model="concurrentConnections"
+        ></v-text-field>
       </v-col>
       <v-col cols="4">
         <label>Compression</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <v-select label="Compression"></v-select>
+        <v-select
+          label="Compression"
+          v-model="compression"
+          :items="[
+            'No preference',
+            'Disable-No Compression',
+            'Enabled with Adaptive Compression',
+            'Enabled without Adaptive Compression',
+          ]"
+        ></v-select>
       </v-col>
 
       <v-col cols="4">
         <label>Type of service</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+        <input type="checkbox" v-model="typefService" />
         <label class="ml-2">Active type of service</label>
       </v-col>
       <v-col cols="4">
         <label>Connections</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+        <input type="checkbox" v-model="Connections" />
         <label class="ml-2">Duplicate connections</label>
       </v-col>
       <v-col cols="4">
         <label>IPv6</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+        <input type="checkbox" v-model="IPv6" />
         <label class="ml-2">Disable IPv6</label>
       </v-col>
       <v-col cols="4">
         <label>Inter clients</label>
       </v-col>
       <v-col cols="8" class="mb-n6">
-        <input type="checkbox" />
+        <input type="checkbox" v-model="interClients" />
         <label class="ml-2">Communication inter clients</label>
       </v-col>
     </v-row>
   </div>
 </template>
+<script setup>
+import { useVModels } from "@vueuse/core";
+
+const props = defineProps([
+  "deviceMode",
+  "ip4Tunnel",
+  "ip6Tunnel",
+  "isGateway",
+  "isBridge",
+  "interfaceBridge",
+  "startDHCPBridge",
+  "endDHCPBridge",
+  "iPv4Local",
+  "iPv6Local",
+  "iPv4Remote",
+  "iPv6Remote",
+  "concurrentConnections",
+  "compression",
+  "typefService",
+  "Connections",
+  "IPv6",
+  "interClients",
+]);
+const emit = defineEmits([
+  "update:ip6Tunnel",
+  "update:isGateway",
+  "update:isBridge",
+  "update:interfaceBridge",
+  "update:startDHCPBridge",
+  "update:endDHCPBridge",
+  "update:iPv4Local",
+  "update:iPv6Local",
+  "update:iPv4Remote",
+  "update:iPv6Remote",
+  "update:concurrentConnections",
+  "update:compression",
+  "update:typefService",
+  "update:Connections",
+  "update:IPv6",
+  "update:interClients",
+  "update:ip4Tunnel",
+]);
+const {
+  ip6Tunnel,
+  isGateway,
+  isBridge,
+  startDHCPBridge,
+  interfaceBridge,
+  endDHCPBridge,
+  iPv4Local,
+  iPv6Local,
+  iPv4Remote,
+  iPv6Remote,
+  concurrentConnections,
+  compression,
+  typefService,
+  Connections,
+  IPv6,
+  interClients,
+  ip4Tunnel,
+} = useVModels(props, emit);
+</script>

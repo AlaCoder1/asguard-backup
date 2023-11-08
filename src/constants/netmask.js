bcaeb6ad3ac7410798658c0be0404e6636f3578e
ines.bouguerra <i.bouguerra@numeryx.fr>
2023-11-08 12:18:26 +0000

    wip : network optimisation and refactoring

---
+++ src/constants/netmask.js
@@ -0,0 +1,2 @@
+
+const netmasks = Array.from({ length: 32 }, (_, i) => i + 1);
--- src/views/network/ifName.vue
+++ src/views/network/ifName.vue
@@ -2,20 +2,13 @@
   <v-app id="inspire">
     <base-layout title="List of interface">
       <template #content>
-        <!-- <v-tabs 
-         background-color="#fff" color="#FFC300" dark @change="handleTabChange">
-          <v-tab v-for="tab in tabs" :key="tab.id">
-             <span style="color: #020202;">{{ tab.name_interface }}</span>
-          </v-tab>
-          <v-tab-item v-for="tab in tabs" :key="tab.name_interface">
-            <IfNameComponent :id="tab.name_interface" :activeTab="activeTabValue" />
-          </v-tab-item>
-        </v-tabs> -->
-
-
         <v-tabs
-        v-model="selectedTab"
-        background-color="#fff" color="#FFC300" dark @change="handleTabChange"
+          v-model="selectedTab"
+          background-color="#f5f5f5"
+          color="black"
+          :class="{'elevation-0': true}"
+          :slider-color="'#FFC300'"
+          @change="handleTabChange"
         >
           <v-tab v-for="tab in tabs" :key="tab.id">
             <span style="color: #020202">{{ tab.name_interface }}</span>
@@ -23,81 +16,83 @@
         </v-tabs>
 
         <v-window v-model="selectedTab">
-          <v-window-item v-for="tab in tabs" :key="tab.name_interface" value="LAN">
-            <v-card>
-              <v-card-text> <IfNameComponent :id="tab.name_interface" :activeTab="activeTabValue" /></v-card-text>
-            </v-card>
+          <v-window-item
+            v-for="tab in tabs"
+            :key="tab.name_interface"
+            :value="tab.name_interface"
+          >
+            <IfNameComponent
+              :id="tab.name_interface"
+              :activeTab="selectedTab"
+            />
           </v-window-item>
         </v-window>
       </template>
-    </base-layout>  
+    </base-layout>
   </v-app>
 </template>
 
 <script>
-import BaseLayout from '../../layouts/layout.vue';
-import IfNameComponent from '../../views/network/components/IfNameComponent.vue';
+import BaseLayout from "../../layouts/layout.vue";
+import IfNameComponent from "../../views/network/components/IfNameComponent.vue";
 
 export default {
   components: {
     BaseLayout,
-    IfNameComponent
+    IfNameComponent,
   },
   data() {
     return {
-      selectedTab:'LAN',
+      selectedTab: "",
       activeTabValue: "",
       interfaces: [],
       IPV4Config: {},
       allStaticGateways: [],
     };
   },
-   methods: {
-     handleTabChange(newTabValue) {
-       this.activeTabValue = this.tabs[newTabValue].name_interface;
+  methods: {
+    handleTabChange(newTabValue) {
+      this.activeTabValue = this.tabs[newTabValue].name_interface;
     },
   },
   computed: {
     tabs() {
-      return this.interfaces.map(element => {
+      return this.interfaces.map((element) => {
         return {
           name_interface: element.name_interface,
         };
       });
-      
     },
   },
-  mounted() {
- 
-      
-
-    this.interfaces = document.getElementById("app").attributes["interfaces"].value;
+  beforeMount: async function () {
+    this.interfaces =
+      document.getElementById("app").attributes["interfaces"].value;
     let validJsonString = this.interfaces
       .replace(/'/g, '"')
-      .replace(/True/g, 'true')
-      .replace(/False/g, 'false')
-      .replace(/None/g, 'null');
+      .replace(/True/g, "true")
+      .replace(/False/g, "false")
+      .replace(/None/g, "null");
     let parsedArray = JSON.parse(validJsonString);
     this.interfaces = parsedArray;
 
-    this.IPV4Config = document.getElementById("app").attributes["IPV4Config"].value;
-    validJsonString = this.IPV4Config
-      .replace(/'/g, '"')
-      .replace(/True/g, 'true')
-      .replace(/False/g, 'false')
-      .replace(/None/g, 'null');
+    this.IPV4Config =
+      document.getElementById("app").attributes["IPV4Config"].value;
+    validJsonString = this.IPV4Config.replace(/'/g, '"')
+      .replace(/True/g, "true")
+      .replace(/False/g, "false")
+      .replace(/None/g, "null");
     parsedArray = JSON.parse(validJsonString);
     this.IPV4Config = parsedArray;
 
-    this.allStaticGateways = document.getElementById("app").attributes["allStaticGateways"].value;
+    this.allStaticGateways =
+      document.getElementById("app").attributes["allStaticGateways"].value;
     validJsonString = this.allStaticGateways
       .replace(/'/g, '"')
-      .replace(/True/g, 'true')
-      .replace(/False/g, 'false')
-      .replace(/None/g, 'null');
+      .replace(/True/g, "true")
+      .replace(/False/g, "false")
+      .replace(/None/g, "null");
     parsedArray = JSON.parse(validJsonString);
     this.allStaticGateways = parsedArray;
   },
 };
 </script>
-

ecbbc37bf250afbeaf7820a9886d656839bff7a2
Akram <a.baklouti@numeryx.fr>
2023-11-15 08:46:33 +0100

    create static dh files and load them in creating server

--- backend/openvpn/server_openvpn.py
+++ backend/openvpn/server_openvpn.py
@@ -1,9 +1,9 @@
 import shutil
 from backend.managementCertificates.functions import initialize_ca
-from backend.openvpn.functions import create_tls_file, execute_command_with_arguments, execute_list_commands_without_arguments, get_current_directory
+from backend.openvpn.functions import create_tls_file, execute_list_commands_without_arguments, get_current_directory
 
 
-def install_server_openvpn(server_name, ca_name, dh_length, tls_auth, server_conf:str):
+def install_server_openvpn(server_name, ca_name, tls_auth, server_conf:str):
     """Function to install an openvpn server in system using easyrsa package to generate keys and certificates"""
     current_dir = get_current_directory()
     
@@ -14,9 +14,7 @@ def install_server_openvpn(server_name, ca_name, dh_length, tls_auth, server_con
     with open(f'/etc/openvpn/server/server_{server_name}.conf', 'w') as server_file:
         server_file.write(server_conf)
 
-    commands_list_without_arguments = [['sudo', 'easyrsa', 'gen-dh', f'{dh_length}'],
-                                       ['cp', f'{current_dir}/pki/dh.pem', f'/etc/openvpn/server/dh_{server_name}.pem'],
-                                       ['sudo', 'mkdir', '-p', '/var/log/openvpn/'],
+    commands_list_without_arguments = [['sudo', 'mkdir', '-p', '/var/log/openvpn/'],
                                        ['sudo', 'touch', '/var/log/openvpn/status.log'],
                                        ['sudo', 'sudo', 'chown', '777', '/var/log/openvpn/status.log'],
                                        ]
@@ -35,25 +33,16 @@ def delete_server_openvpn(server_name):
     """Function to delete an openvpn server in system and his keys and certificates"""
     commands_list_without_arguments = [['sudo', 'systemctl', 'stop', f'openvpn-server@server_{server_name}'],
                                        ['sudo', 'rm', '-f', f'/etc/openvpn/server/server_{server_name}.conf'],
-                                       ['sudo', 'rm', '-f', f'/etc/openvpn/server/dh_{server_name}.pem'],
                                        ['sudo', 'rm', '-f', f'/etc/openvpn/server/static_{server_name}.key'],
                                        ['sudo', 'rm', '-f', f'/var/log/openvpn/status-server_{server_name}.log'],
                                        ]
     execute_list_commands_without_arguments(commands_list_without_arguments)
 
 
-def update_server_openvpn(server_name, dh_length, tls_auth, server_conf):
+def update_server_openvpn(server_name, tls_auth, server_conf):
     """Function to update an openvpn server in system"""
-    # current_dir = get_current_directory()
-    
-    # # Initialization
-    # initialize_ca(current_dir)
 
     create_tls_file(tls_auth, f'/etc/openvpn/server/static_{server_name}.key')
     
     with open(f'/etc/openvpn/server/server_{server_name}.conf', 'w') as server_file:
         server_file.write(server_conf)
-
-    if dh_length:
-        commands_list_without_arguments = [['sudo', 'easyrsa', 'gen-dh', f'{dh_length}']]
-        execute_list_commands_without_arguments(commands_list_without_arguments)

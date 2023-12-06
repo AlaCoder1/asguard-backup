0e3c7edd56f1d58f0e37d5ab47d4a38e61232029
A.S <s.aloui @numeryx.fr>
2023-12-06 16:47:41 +0100

    Integration Squid UI

--- asguard/urls.py
+++ asguard/urls.py
@@ -63,9 +63,11 @@ urlpatterns = [
     path('openvpn/', include('backend.openvpn.urls')),
     path('ipsec/', include('backend.ipsec.urls')),
     path('ipsec/', ipsec_page),
+    path('proxy-web/', squid_proxy),
     path('rules/', include('backend.rules.urls')),
     path('gateway/', include('backend.gateway.urls')),
     path("monitoring/",include("backend.dashboard.urls"))
+    
 ]
 
 # ws/wss url patterns
---
+++ src/middleware/squid.js
@@ -0,0 +1,29 @@
+import { createApp } from "vue";
+import store from "../store/index.js";
+import "vuetify/styles";
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import squid from "../views/squid/index.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import axios from "axios";
+const app = createApp(squid);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401 || error.response.status === 403) {
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+app.use(ElementPlus).use(store).use(vuetify).mount("#app");

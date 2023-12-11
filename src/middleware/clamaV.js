2ec1474fd04443db960eb2fc739e52325d4e0b51
A.S <s.aloui @numeryx.fr>
2023-12-11 14:32:36 +0100

    Add ClamaV Config Ui

---
+++ src/middleware/clamaV.js
@@ -0,0 +1,32 @@
+import { createApp } from "vue";
+import store from "../store/index.js";
+import "vuetify/styles";
+import mitt from 'mitt'
+import { createVuetify } from "vuetify";
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import clamaV from "../views/clamaV/index.vue";
+import ElementPlus from "element-plus";
+import "element-plus/dist/index.css";
+import axios from "axios";
+const app = createApp(clamaV);
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+const emitter = mitt()
+app.provide('emitter', emitter)
+
+axios.interceptors.response.use(
+  (response) => {
+    return response;
+  },
+  (error) => {
+    if (error.response.status === 401 || error.response.status === 403) {
+      window.location.href = "/";
+    }
+    return Promise.reject(error);
+  }
+);
+
+app.use(ElementPlus).use(store).use(vuetify).mount("#app");

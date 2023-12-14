e3f5185cb96cd1e36c102f7f837cefbcaca817f8
HT <h.touzi@numeryx.fr>
2023-12-14 15:25:14 +0100

    Update api get clamav

--- backend/clamav/urls.py
+++ backend/clamav/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-     path('GetConfigurations', views.getConfiguratiosnFromDatabase, name="Get_Configurations"),
+     path('GetConfigurations', views.getconfiguratiosnfromdatabase, name="Get_Configurations"),
      path('updateClamav/<int:id>', views.update_clamav_configuration, name="Update_Configurations"),
      path('Updatefreshclam', views.update_freshclam_database, name="update_freshclam"),
 ]

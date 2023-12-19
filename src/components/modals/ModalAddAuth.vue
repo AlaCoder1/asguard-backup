91bb6aaf79a20da01e01667fba11f2d07066aeb4
ines.bouguerra <i.bouguerra@numeryx.fr>
2023-12-19 09:16:25 +0000

    remove required privateKey from certificates

--- src/components/modals/ModalAddAuth.vue
+++ src/components/modals/ModalAddAuth.vue
@@ -79,12 +79,6 @@
                     label="Private key certificate (facultatif)"
                     variant="outlined"
                   ></v-textarea>
-                  <p
-                    class="error-feedback mb-5"
-                    v-if="v$.formData.privateKey.$error"
-                  >
-                    {{ v$.formData.privateKey.$errors[0].$message }}
-                  </p>
 
                   <v-text-field
                     label="Serial number certificate"
@@ -359,16 +353,6 @@ export default {
           },
           method: { required },
           certificatData: {
-            requiredIfFuction: helpers.withMessage(
-              "Value is required",
-              requiredIf(
-                () =>
-                  state.formData.method.name ===
-                  "Import an existing Certificate Authority"
-              )
-            ),
-          },
-          privateKey: {
             requiredIfFuction: helpers.withMessage(
               "Value is required",
               requiredIf(

211dfb0016095bd7930b2165fdb3575dad069438
A.S <s.aloui @numeryx.fr>
2024-01-04 10:04:01 +0100

    Add Squid Server Alert

--- views/views.py
+++ views/views.py
@@ -567,8 +567,8 @@ def squid_proxy(request):
     statusEnable = statusEnableAuth(request)
     proxyRule = get_all_proxy_rules(request)
     proxyGroups = allGProxyroups(request)
-    context = {'proxyUser': json.dumps(proxyUser),'generalInfo' : json.dumps(generalInfo),'statusEnable' : json.dumps(statusEnable),'proxyRule' : json.dumps(proxyRule),'proxyGroups' : json.dumps(proxyGroups)}
-    print('-------------:',proxyGroups)
+    statusServer = get_squid_status_from_bd()
+    context = {'proxyUser': json.dumps(proxyUser),'generalInfo' : json.dumps(generalInfo),'statusEnable' : json.dumps(statusEnable),'proxyRule' : json.dumps(proxyRule),'proxyGroups' : json.dumps(proxyGroups),'statusServer' : json.dumps(statusServer)}
     return render(request, 'squid_proxy.html',context)
 
 

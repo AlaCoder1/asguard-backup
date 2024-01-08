8edd9aa8dbaaa5c532e501c582ca179e6f9894f1
a.bouhrira <a.bouhrira@numeryx.fr>
2024-01-08 09:01:45 +0100

    separate add and update rule

--- backend/rules/urls.py
+++ backend/rules/urls.py
@@ -6,4 +6,9 @@ urlpatterns = [
      path('GetAllRules', views.GetAllRules, name="GetAllRules"),
      path('deleteRule/<int:id>', views.deleteRule, name="deleteRule"),
      path('saveRules/<str:name_interface>', views.saveRules, name="saveRules"),
+     path('addRule/<str:name_interface>', views.addRule, name="addRule"),
+     path('updateRule/<str:name_interface>', views.updateRule, name="updateRule"),
+     
+     
+     
 ]

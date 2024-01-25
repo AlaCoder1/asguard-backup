e4184630538f6f98a1bcb59d2417948a5544c20e
a.bouhrira <a.bouhrira@numeryx.fr>
2024-01-25 15:02:18 +0100

    Revert "fixbug authentication"
    This reverts commit bff632fb886042d9163b5400a53fc2c504894f34.

--- backend/rules/urls.py
+++ backend/rules/urls.py
@@ -5,6 +5,7 @@ from . import views
 urlpatterns = [
      path('GetAllRules', views.GetAllRules, name="GetAllRules"),
      path('deleteRule/<int:id>', views.deleteRule, name="deleteRule"),
+     path('saveRules/<str:name_interface>', views.saveRules, name="saveRules"),
      path('addRule/<str:name_interface>', views.addRule, name="addRule"),
      path('updateRule/<str:name_interface>', views.updateRule, name="updateRule"),
      
--- views/views.py
+++ views/views.py
@@ -593,10 +593,17 @@ def subscription_page(request):
 
 
 def login(request):
+    usr=getAllUsers(request)
+    print (usr)
+
+    context = {'users':usr}
     if request.user.is_authenticated:
+        next_url = request
+        # index_page(request)
+        print('path+***********: ',request.path)
         return redirect('/dashboard/')
     else:
-        return render(request, 'login.html')
+        return render(request, 'login.html',context)
 
 
 @login_required(login_url='/')

4f7cf84a0e49f3f01fd295341214b71fede077d3
a.bouhrira <a.bouhrira@numeryx.fr>
2024-02-29 16:49:39 +0100

    config suricata with yaml

--- backend/ids_ips/urls.py
+++ backend/ids_ips/urls.py
@@ -3,19 +3,9 @@ from . import views
 
  
 urlpatterns = [
-    ##Other
-    # path('getSuricataFile', views.getSuricataFile, name="getSuricataFile"),
-    # path('update-suricata-status/<int:id>', views.update_suricata_status, name='update-suricata-status'),
-    # path('get-suricata-status/<int:id>', views.get_suricata_status, name='get-suricata-status'),
-    # path('updateRule/<int:sid>', views.updateRule, name="updateRule"),
-    # path('statusRule/<int:sid>', views.update_status_rule, name="statusRule"),
-    # path('getRuleStatus/<int:sid>', views.getRuleStatus, name="getRuleStatus"),
-    # path('addDefaultRulesToDatabase/<int:id>', views.addDefaultRulesToDatabase, name='addDefaultRulesToDatabase'),
-    # path('addGeneralConfig', views.addGeneralConfig, name='addGeneralConfig'),
-     ##Other
+  
     ###########config
     path('UpdateGeneralConfig/<int:id>', views.update_suricata_configuration, name='UpdateGeneralConfig'),
-    path('get_suricata_configuration/<int:id>', views.get_suricata_configuration, name='get_suricata_configuration'),
     ########### end config
     ###########Rules
     path('activerSuricataUpdate/<int:id>', views.activer_suricata_update, name='activerSuricataUpdate'),
--- backend/tasks/management/commands/init_suricata_file.py
+++ backend/tasks/management/commands/init_suricata_file.py
@@ -1,96 +1,25 @@
 from django.core.management.base import BaseCommand
 from django.db import IntegrityError
-
-from backend.ids_ips.function_sys import execute_cmd, save_config
+from backend.ids_ips.function_sys import  read_from_yaml, save_in_yaml, update_config
+import ruamel.yaml
 
 
 class Command(BaseCommand):
-    def find_file_log(self,stripped_line,updated_lines,line,new_file_log,log_type):
-        next_log=False
-        next_filename=False
-        if "stats:" in stripped_line:
-            next_log = True  
-            updated_lines.append(line + '\n') 
-        elif next_log:
-            if "enabled:" in stripped_line:
-                next_filename=True
-                updated_lines.append(line + '\n') 
-            elif next_filename:
-                updated_lines.append(f'    filename: {new_file_log}\n')
-                next_filename = False
-                next_log=False
-        return updated_lines
-    def update_filenames(self,lines):
-        try:
-            updated_lines = []
-            next_eve_log = False
-            next_fastlog = False
-            next_stats=False
-            next_filename=False
-            new_eve_log="/var/log/suricata/eve.json"
-            new_fastlog="/var/log/suricata/fast.log"
-            stats_log="/var/log/suricata/stats.log"
-            # Ajout de la logique de testa() ici
-            for line in lines:
-                stripped_line = line.strip()
-                if stripped_line.startswith("#"):
-                    updated_lines.append(line + '\n')
-                # elif "stats:" in stripped_line:
-                #     next_stats = True  
-                #     updated_lines.append(line + '\n') 
-                # elif next_stats:
-                #     if "enabled:" in stripped_line:
-                #         next_stats=True
-                #         updated_lines.append(line + '\n') 
-                #     elif next_stats:
-                #         updated_lines.append(f'    filename: {stats_log}\n')
-                #         next_stats=False
-                    
-                ####configgg eve.log
-                elif "eve-log:" in stripped_line:
-                    next_eve_log = True  
-                    updated_lines.append(line + '\n') 
-                elif next_eve_log:
-                    if "filetype:" in stripped_line:
-                        next_filename=True
-                        updated_lines.append(line + '\n') 
-                    elif next_filename:
-                        updated_lines.append(f'      filename: {new_eve_log}\n')
-                        next_eve_log=False
-                        next_filename=False
-                          ####configgg fast.log
-                elif "fast:" in stripped_line:
-                    next_fastlog = True  
-                    updated_lines.append(line + '\n') 
-                elif next_fastlog:
-                    if "enabled:" in stripped_line:
-                        updated_lines.append(line + '\n') 
-                    elif next_fastlog:
-                        updated_lines.append(f'      filename: {new_fastlog}\n')
-                        next_fastlog=False
-               
-                else:
-                  
-                    # Conserve les autres lignes telles quelles
-                    updated_lines.append(line + '\n')
-            return updated_lines
-        except Exception:
-            # Capture toute autre exception et affiche un message d'erreur
-            return None
-        
     def handle(self, *args, **kwargs):
         try:
-            suricata_yaml_path="/etc/suricata/suricata.yaml"
-            output,_= execute_cmd("sudo cat " + suricata_yaml_path)
-            status_enabled=True
-            if output:
-                lines = output.split('\n')
-                updated_lines=self.update_filenames(lines)
-                # self.save_changes(updated_lines,suricata_yaml_path)
-                aux_save=save_config(updated_lines,suricata_yaml_path,status_enabled)
-                if aux_save is True:     
-                    return "Config saved successfully!"
-                else:
-                    return aux_save
+            status_enabled=False
+            global_path="/var/log/suricata/"
+            yaml_class = ruamel.yaml.YAML()
+            suricata_path="/etc/suricata/suricata.yaml"
+            data=read_from_yaml(suricata_path,yaml_class)
+            data['outputs'][1]["eve-log"]['filename']=global_path+data['outputs'][1]["eve-log"]['filename']
+            data['outputs'][7]['stats']['filename']=global_path+data['outputs'][7]['stats']['filename']
+            data['outputs'][0]['fast']['filename']=global_path+data['outputs'][0]['fast']['filename']
+            save_in_yaml(suricata_path,data,yaml_class) 
+            aux_update_system=update_config(status_enabled)
+            if aux_update_system is True:
+                return "Config saved successfully"
+            else:
+                return aux_update_system
         except IntegrityError as e:
             return "Error: " + str(e)    

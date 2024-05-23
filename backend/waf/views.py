53b23a4c8763f2a58e0fadd1239f6f3e991286c9
Akram <a.baklouti@numeryx.fr>
2024-05-23 06:35:58 +0100

    add application feature for WAF and fix rules payload

--- backend/waf/views.py
+++ backend/waf/views.py
@@ -1,18 +1,40 @@
 from django.http import JsonResponse
+from django.utils.translation import gettext_lazy as _
 from drf_yasg.utils import swagger_auto_schema
 from rest_framework.authentication import SessionAuthentication
 from rest_framework.decorators import api_view, permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
-from drf_yasg.openapi import Schema, TYPE_BOOLEAN, TYPE_OBJECT, TYPE_STRING, TYPE_INTEGER
+from drf_yasg.openapi import Schema, TYPE_ARRAY, TYPE_BOOLEAN, TYPE_OBJECT, TYPE_STRING, TYPE_INTEGER
 
-from backend.waf.constant_variables import CONSTANT_WAF_CONFIG, CONSTANT_WAF_RULE
-from backend.waf.list_waf import get_list_all_waf, get_one_waf, get_one_waf_config
-from backend.waf.models import ConfigWaf
-from backend.waf.serializers import ConfigWafSerializer, RulesWafSerializer
-from backend.waf.utils import change_waf_config_file
-from backend.waf.utils_system import create_rule_waf_in_system
-from utils.constant_variables import ERROR_MESSAGES_CREATING, ERROR_MESSAGES_UPDATING, SUCCESS_MESSAGES_CREATING_ITEM, SUCCESS_MESSAGES_UPDATE
+from backend.waf.list_waf import get_list_all_waf_rule, get_list_all_waf_application, get_one_waf_rule, get_one_waf_application, get_one_waf_config
+from backend.waf.models import ApplicationWaf, ConfigWaf, RulesWaf
+from backend.waf.serializers import ApplicationWafSerializer, ConfigWafSerializer, RulesWafSerializer
+from backend.waf.utils import convert_waf_rule_payload
+from backend.waf.utils_application import create_application_waf_in_system, delete_application_waf_in_system, update_application_waf_in_system
+from backend.waf.utils_config import change_waf_config_file
+from backend.waf.utils_rules import create_rule_waf_in_system, delete_rule_waf_in_system, start_rule_in_system, stop_rule_in_system, update_rule_waf_in_system
 from utils.errors_utils import CommandExecutionError
+
+
+# Constants
+CONSTANT_WAF_CONFIG = "WAF Config"
+CONSTANT_WAF_RULE = "WAF Rule"
+CONSTANT_WAF_APPLICATION = "WAF Application"
+# Success messages
+SUCCESS_MESSAGES_CREATING = _("is created")
+SUCCESS_MESSAGES_DELETING = _("is deleted")
+SUCCESS_MESSAGES_UPDATING = _("is updated")
+SUCCESS_MESSAGES_STARTING = _("is started")
+SUCCESS_MESSAGES_STOPING = _("is stoped")
+# Error messages
+ERROR_MESSAGES_CREATING = _("Error in creating")
+ERROR_MESSAGES_DELETING = _("Error in deleting")
+ERROR_MESSAGES_UPDATING = _("Error in updating")
+ERROR_MESSAGES_STARTING = _("Error in starting")
+ERROR_MESSAGES_STOPING = _("Error in stoping")
+ERROR_MESSAGES_EXPORTING = _("Error in exporting")
+ERROR_MESSAGES_DELETING_USED_ITEM = _("Unable to delete")
+ERROR_MESSAGES_INEXISTANT = _("does not exist")
 
 
 ########################################
@@ -30,8 +52,9 @@ def get_waf_config(request):
 
 
 @swagger_auto_schema('PUT', responses={200: 'Created', 400: 'Bad Request'}, 
-                     operation_summary="API TO UPDATE THE WAF CONFIG", request_body=Schema(
-                         type=TYPE_OBJECT, 
+                     operation_summary="API TO UPDATE THE WAF CONFIG", 
+                     request_body=Schema(
+                         type=TYPE_OBJECT,
                          required=["rule_engine_initialization", "access_request_bodies", "xml_request_body_parser", 
                                    "json_request_body_parser", "maximum_request_body_size", "request_body_size_files_excluded",
                                    "request_body_limit_action", "maximum_parsing_depth_json", "maximum_number_args_request", 
@@ -58,7 +81,7 @@ def get_waf_config(request):
 @authentication_classes([SessionAuthentication])
 @permission_classes([IsAuthenticated])
 def update_config_waf(request, id):
-    """Creating a new Route and adding it to the database"""
+    """Updating WAF Configuration"""
     try:
         data = request.data
         config_waf = ConfigWaf.objects.get(id=id)
@@ -66,11 +89,11 @@ def update_config_waf(request, id):
         config_waf_serializer = ConfigWafSerializer(config_waf, data=data)
         if config_waf_serializer.is_valid():
             config_waf_serializer.save()
-            return JsonResponse({"msg": SUCCESS_MESSAGES_UPDATE.format(CONSTANT_WAF_CONFIG)}, status=200)
+            return JsonResponse({"msg": f"{CONSTANT_WAF_CONFIG} {SUCCESS_MESSAGES_UPDATING}"}, status=200)
         return JsonResponse({"error": list(config_waf_serializer.errors.values())[0][0]}, status=400)
         
     except CommandExecutionError:
-        return JsonResponse({"error": ERROR_MESSAGES_UPDATING.format(CONSTANT_WAF_CONFIG)}, status=400)
+        return JsonResponse({"error": f"{ERROR_MESSAGES_UPDATING} {CONSTANT_WAF_CONFIG}"}, status=400)
 
 
 ########################################
@@ -84,7 +107,7 @@ def update_config_waf(request, id):
 def get_all_waf_rule(request):
     """Getting all WAF Rules from database"""
     list_snat = []
-    list_snat = get_list_all_waf()
+    list_snat = get_list_all_waf_rule()
     return JsonResponse(list_snat, safe=False)
 
 
@@ -95,14 +118,21 @@ def get_all_waf_rule(request):
 @permission_classes([IsAuthenticated])
 def get_waf_rule(request, id):
     """Getting WAF by id from database"""
-    snat = get_one_waf(id)
+    snat = get_one_waf_rule(id)
     return JsonResponse(snat, safe=False)
 
 
 @swagger_auto_schema('POST', responses={200: 'Created', 400: 'Bad Request'}, 
                      operation_summary="API TO CREATE A WAF RULE", request_body=Schema(
-                         type=TYPE_OBJECT, required=['interface'],
-                         properties={'interface': Schema(type=TYPE_INTEGER, description="Id of the interface"),
+                         type=TYPE_OBJECT, required=['name', 'variables', 'operators', 'transformations', 'actions'],
+                         properties={'name': Schema(type=TYPE_STRING, description="Name of the rule"),
+                                     'variables': Schema(type=TYPE_ARRAY, items=Schema(type=TYPE_STRING)),
+                                     'operators': Schema(type=TYPE_ARRAY, items=Schema(type=TYPE_STRING)),
+                                     'transformations': Schema(type=TYPE_ARRAY, items=Schema(type=TYPE_STRING)),
+                                     'action': Schema(type=TYPE_ARRAY, description= "id action is mandatory. If an action don't have a value like pass or log then value will be an empty string", 
+                                                      items=Schema(type=TYPE_OBJECT, required=['type', 'value'], properties={
+                                                          'type': Schema(type=TYPE_STRING),
+                                                          'value': Schema(type=TYPE_STRING)})),
                                      }
                                      ))
 @api_view(['POST'])
@@ -112,6 +142,7 @@ def create_waf_rule(request):
     """Creating a new WAF Rule and adding it to the database"""
     try:
         data = request.data
+        data = convert_waf_rule_payload(data)
         
         serializer_rule_waf = RulesWafSerializer(data=data)
         if serializer_rule_waf.is_valid():
@@ -123,9 +154,236 @@ def create_waf_rule(request):
 
                 # Add the rule to the database
                 serializer_rule_waf.save()
-                return JsonResponse({"msg": SUCCESS_MESSAGES_CREATING_ITEM.format(CONSTANT_WAF_RULE, "")}, status=201)
+                return JsonResponse({"msg": f"{CONSTANT_WAF_RULE} {SUCCESS_MESSAGES_CREATING}"}, status=201)
 
         return JsonResponse({"error": list(serializer_rule_waf.errors.values())[0][0]}, status=400)
         
     except CommandExecutionError:
-        return JsonResponse({"error": ERROR_MESSAGES_CREATING.format(CONSTANT_WAF_RULE)}, status=400)
+        return JsonResponse({"error": f"{ERROR_MESSAGES_CREATING} {CONSTANT_WAF_RULE}"}, status=400)
+
+
+@swagger_auto_schema('DELETE', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO DELETE A WAF RULE",)
+@api_view(['Delete'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def delete_waf_rule(request, id):
+    """Deleting a waf_rule from database"""
+    try:
+        waf_rule = RulesWaf.objects.get(id=id)
+
+        # Delete rule from system
+        delete_rule_waf_in_system(waf_rule)
+
+        # delete rule from database
+        waf_rule.delete()
+        return JsonResponse({"msg": f"{CONSTANT_WAF_RULE} {SUCCESS_MESSAGES_DELETING}"}, status=201)
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_DELETING} {CONSTANT_WAF_RULE}"}, status=400)
+    except RulesWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_RULE} {ERROR_MESSAGES_INEXISTANT}"}, status=400)
+
+
+@swagger_auto_schema('PUT', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO UPDATE A WAF RULE", request_body=Schema(
+                         type=TYPE_OBJECT, required=['interface'],
+                         properties={'interface': Schema(type=TYPE_INTEGER, description="Id of the interface"),
+                                     }
+                                     ))
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def update_waf_rule(request, id):
+    """Updating a WAF Rule"""
+    try:
+        waf_rule = RulesWaf.objects.get(id=id)
+        data = request.data
+        data = convert_waf_rule_payload(data)
+        
+        serializer_rule_waf = RulesWafSerializer(waf_rule, data=data)
+        if serializer_rule_waf.is_valid():
+
+            rule_waf = update_rule_waf_in_system(waf_rule.rule_content, data)
+            data["rule_content"] = rule_waf
+            serializer_rule_waf = RulesWafSerializer(waf_rule, data=data)
+            if serializer_rule_waf.is_valid():
+
+                # Add the rule to the database
+                serializer_rule_waf.save()
+                return JsonResponse({"msg": f"{CONSTANT_WAF_RULE} {SUCCESS_MESSAGES_UPDATING}"}, status=201)
+
+        return JsonResponse({"error": list(serializer_rule_waf.errors.values())[0][0]}, status=400)
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_UPDATING} {CONSTANT_WAF_RULE}"}, status=400)
+    except RulesWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_RULE} {ERROR_MESSAGES_INEXISTANT}"}, status=400)
+
+
+@swagger_auto_schema('PUT', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO START A WAF RULE")
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def start_waf_rule(request, id):
+    """Starting a WAF Rule"""
+    try:
+        waf_rule = RulesWaf.objects.get(id=id)
+        if not waf_rule.rule_status:
+            start_rule_in_system(waf_rule)
+            waf_rule.rule_status = True
+            waf_rule.save()
+            return JsonResponse({"msg": f"{CONSTANT_WAF_RULE} {SUCCESS_MESSAGES_STARTING}"})
+        return JsonResponse({"error": f"{ERROR_MESSAGES_STARTING} {CONSTANT_WAF_RULE}"})
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_STARTING} {CONSTANT_WAF_RULE}"}, status=400)
+    except RulesWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_RULE} {ERROR_MESSAGES_INEXISTANT}"}, status=400)
+
+
+@swagger_auto_schema('PUT', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO STOP A WAF RULE")
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def stop_waf_rule(request, id):
+    """Stoping a WAF Rule"""
+    try:
+        waf_rule = RulesWaf.objects.get(id=id)
+        if waf_rule.rule_status:
+            stop_rule_in_system(waf_rule)
+            waf_rule.rule_status = False
+            waf_rule.save()
+            return JsonResponse({"msg": f"{CONSTANT_WAF_RULE} {SUCCESS_MESSAGES_STOPING}"})
+        return JsonResponse({"error": f"{ERROR_MESSAGES_STARTING} {CONSTANT_WAF_RULE}"})
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_STOPING} {CONSTANT_WAF_RULE}"}, status=400)
+    except RulesWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_RULE} {ERROR_MESSAGES_INEXISTANT}"}, status=400)
+
+
+########################################
+########### WAF Application ############
+########################################
+@swagger_auto_schema('GET', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO GET LIST OF ALL WAF APPLICATIONS",)
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def get_all_waf_application(request):
+    """Getting all waf applications from database"""
+    list_waf_application = []
+    list_waf_application = get_list_all_waf_application()
+    return JsonResponse(list_waf_application, safe=False)
+
+
+@swagger_auto_schema('GET', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO GET A WAF APPLICATION",)
+@api_view(['GET'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def get_waf_application(request, id):
+    """Getting waf application by id from database"""
+    waf_application = get_one_waf_application(id)
+    return JsonResponse(waf_application, safe=False)
+
+
+@swagger_auto_schema('POST', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO CREATE A WAF APPLICATION", request_body=Schema(
+                         type=TYPE_OBJECT, required=['name', 'application_type', 'application_value', 'description', 'rules'],
+                         properties={'name': Schema(type=TYPE_STRING, description="Name of the application"),
+                                     'application_type': Schema(type=TYPE_STRING, enum=['ip', 'domain']),
+                                     'application_value': Schema(type=TYPE_STRING),
+                                     'description': Schema(type=TYPE_STRING),
+                                     'rules': Schema(type=TYPE_ARRAY, description="List of rules object", 
+                                                     items=Schema(type=TYPE_OBJECT, required=['rule_waf', 'rule_policy', 'rule_log'],
+                                                                  properties={'rule_waf': Schema(type=TYPE_INTEGER, description="Id of the rule"),
+                                                                              'rule_policy': Schema(type=TYPE_BOOLEAN, description="True if the user select this rule in Block"),
+                                                                              'rule_log': Schema(type=TYPE_BOOLEAN)}))
+                                     }
+                                     ))
+@api_view(['POST'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def create_waf_application(request):
+    """Creating a new WAF Application and adding it to the database"""
+    try:
+        data = request.data
+        
+        serializer_application_waf = ApplicationWafSerializer(data=data)
+        if serializer_application_waf.is_valid():
+
+            create_application_waf_in_system(data)
+
+            serializer_application_waf.save()
+            return JsonResponse({"msg": f"{CONSTANT_WAF_APPLICATION} {SUCCESS_MESSAGES_CREATING}"}, status=201)
+
+        return JsonResponse({"error": list(serializer_application_waf.errors.values())[0][0]}, status=400)
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_CREATING} {CONSTANT_WAF_APPLICATION}"}, status=400)
+
+
+@swagger_auto_schema('DELETE', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO DELETE A WAF RULE",)
+@api_view(['Delete'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def delete_waf_application(request, id):
+    """Deleting a waf_application from database"""
+    try:
+        waf_application = ApplicationWaf.objects.get(id=id)
+
+        # Delete application from system
+        delete_application_waf_in_system(waf_application)
+
+        # delete application from database
+        waf_application.delete()
+        return JsonResponse({"msg": f"{CONSTANT_WAF_APPLICATION} {SUCCESS_MESSAGES_DELETING}"}, status=201)
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_DELETING} {CONSTANT_WAF_APPLICATION}"}, status=400)
+    except ApplicationWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_APPLICATION} {ERROR_MESSAGES_INEXISTANT}"}, status=400)
+
+
+@swagger_auto_schema('PUT', responses={200: 'Created', 400: 'Bad Request'}, 
+                     operation_summary="API TO UPDATE A WAF APPLICATION", request_body=Schema(
+                         type=TYPE_OBJECT, required=['name', 'application_type', 'application_value', 'description', 'rules'],
+                         properties={'name': Schema(type=TYPE_STRING, description="Name of the application"),
+                                     'application_type': Schema(type=TYPE_STRING, enum=['ip', 'domain']),
+                                     'application_value': Schema(type=TYPE_STRING),
+                                     'description': Schema(type=TYPE_STRING),
+                                     'rules': Schema(type=TYPE_ARRAY, description="List of rules object", 
+                                                     items=Schema(type=TYPE_OBJECT, required=['rule_waf', 'rule_policy', 'rule_log'],
+                                                                  properties={'rule_waf': Schema(type=TYPE_INTEGER, description="Id of the rule"),
+                                                                              'rule_policy': Schema(type=TYPE_BOOLEAN, description="True if the user select this rule in Block"),
+                                                                              'rule_log': Schema(type=TYPE_BOOLEAN)}))
+                                     }
+                                     ))
+@api_view(['PUT'])
+@authentication_classes([SessionAuthentication])
+@permission_classes([IsAuthenticated])
+def update_waf_application(request, id):
+    """Updating a WAF Application"""
+    try:
+        waf_application = ApplicationWaf.objects.get(id=id)
+        data = request.data
+        
+        serializer_application_waf = ApplicationWafSerializer(waf_application, data=data)
+        if serializer_application_waf.is_valid():
+
+            update_application_waf_in_system(waf_application, data)
+
+            serializer_application_waf.save()
+            return JsonResponse({"msg": f"{CONSTANT_WAF_APPLICATION} {SUCCESS_MESSAGES_UPDATING}"}, status=201)
+
+        return JsonResponse({"error": list(serializer_application_waf.errors.values())[0][0]}, status=400)
+        
+    except CommandExecutionError:
+        return JsonResponse({"error": f"{ERROR_MESSAGES_UPDATING} {CONSTANT_WAF_APPLICATION}"}, status=400)
+    except ApplicationWaf.DoesNotExist:
+        return JsonResponse({"error": f"{CONSTANT_WAF_APPLICATION} {ERROR_MESSAGES_INEXISTANT}"}, status=400)

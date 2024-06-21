d534e2df84eca774c720614c351ff743eb7256b9
A.S <s.aloui @numeryx.fr>
2024-06-21 11:37:26 +0100

    api alert

--- src/views/waf/components/wafAlerts.vue
+++ src/views/waf/components/wafAlerts.vue
@@ -114,7 +114,7 @@ export default {
       },
       {
         headerName: countOfRecord,
-        field: "countOfRecord",
+        field: "count_of_record",
         autoHeight: true,
       },
     ]);
@@ -142,7 +142,6 @@ export default {
         autoHeight: true,
         width: 480,
         minWidth: 150,
-        
       },
       {
         headerName: "source",
@@ -162,7 +161,8 @@ export default {
       },
       {
         headerName: "message",
-        field: "message",
+        // field: "message",
+        cellRenderer: formatedMessage,
         autoHeight: true,
         width: 250,
         minWidth: 350,
@@ -176,12 +176,40 @@ export default {
       },
     ]);
 
-    function formatedLine(data) {
+    function formatedMessage(data) {
+      const resultMessage = data.data.message.map((e) => e + "<br>").join("");
+      console.log("resultMessage", resultMessage);
       let eGui = document.createElement("div");
 
-      eGui.innerHTML = `${data.data.violation_id}>${data.data.violation_file}
-        `;
-      eGui.style.lineHeight = "2";
+      eGui.innerHTML = `${resultMessage}`;
+      // eGui.style.lineHeight = "3";
+      return eGui;
+
+      //       const chunks = longString.match(/.{1,20}/g);
+
+      // const resultWithBr = chunks.map((chunk) => chunk + "<br>").join("");
+
+      // let eGui = document.createElement("div");
+
+      // eGui.innerHTML = `${resultWithBr}
+      //   `;
+      // eGui.style.lineHeight = "2";
+    }
+    function formatedLine(data) {
+      // let eGui = document.createElement("div");
+
+      // eGui.innerHTML = `${data.data.violation_id}>${data.data.violation_file}
+      //   `;
+      // eGui.style.lineHeight = "2";
+      // return eGui;
+      const resultMessage = data.data.violation_file
+        .map((e) => e + "<br>")
+        .join("");
+      console.log("resultMessage", resultMessage);
+      let eGui = document.createElement("div");
+
+      eGui.innerHTML = `${resultMessage}`;
+      // eGui.style.lineHeight = "2";
       return eGui;
     }
     const columnCountry = ref([
@@ -276,14 +304,32 @@ export default {
       let wafAlert =
         document.getElementById("app").attributes["waf_alert"].value;
       let waf_alert = JSON.parse(wafAlert);
-      console.log("waf_alert***", waf_alert);
-      rowDataRules.value = waf_alert;
+      
+      rowDataCountry.value = waf_alert?.top_countries
+      rowDataAttacks.value = waf_alert?.attacks
 
-      if (gridApi.value) {
-        gridApi.value.setRowData(rowDataRules.value);
-      } else {
-        console.error("Grid API.");
-      }
+      let test = [
+        {
+          country: "fr",
+          timestamp: "2024-04-17T08:52:43Z",
+          violation_file: [
+            "REQUEST-920-PROTOCOL-ENFORCEMENT.conf00",
+            "REQUEST-920-PROTOCOL-ENFORCEMENT.conf11",
+            "REQUEST-920-PROTOCOL-ENFORCEMENT.conf12",
+          ],
+          violation_id: 920350,
+          source: "10.1.12.74",
+          method: "GET",
+          message: [
+            "Host header is a numeric IP address1",
+            "Host header is a numeric IP address2",
+          ],
+          url: "10.1.12.74/index.html?exec=/bin/bash",
+        },
+      ];
+      rowDataRules.value = test;
+      console.log("waf_alert0***", waf_alert);
+
     });
 
     return {

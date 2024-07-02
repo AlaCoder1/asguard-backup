6482d4726b28d46a93629359980989d2f784ff31
amanibouhrira <a.bouhrira@numeryx.fr>
2024-07-02 16:42:47 +0100

    fixbug str msg

--- backend/openvpn_monitoring/consumers_openvpn.py
+++ backend/openvpn_monitoring/consumers_openvpn.py
@@ -35,9 +35,10 @@ class OpenVpnConsumer(AsyncWebsocketConsumer):
         while True:
             data =await self.start_data_loop_openvpn(id_server,password)
             await self.send(json.dumps(data))
-            await self.save_system_usage(data)
-            await asyncio.sleep(60)
- 
+            if not isinstance(data, str):
+                await self.save_system_usage(data)
+                await asyncio.sleep(60)
+    
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(
             "chart_group_global",

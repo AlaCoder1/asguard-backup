a19d3431efc22f85caf63bc8677c956e816b6c99
Akram <a.baklouti@numeryx.fr>
2024-09-11 10:01:24 +0100

    set architecture of ztna app

--- backend/ztna/utils.py
+++ backend/ztna/utils.py
@@ -1,5 +1,10 @@
+from django.http import JsonResponse
 import requests
 import json
+
+
+BASE_URL = "https://localhost:1280/edge/management/v1/"
+
 
 def get_Zt_Token():
     url = "https://localhost:1280/edge/management/v1/authenticate?method=password"
@@ -24,5 +29,23 @@ def get_Zt_Token():
     else:
         print(f"Failed to authenticate. Status code: {response.status_code}")
         return None
+
+
+def get_data(request, endpoint):
+    url = BASE_URL + endpoint
+    session_id = get_Zt_Token()
+    headers = {"zt-session": session_id}
+
+    params = {
+        "limit": 100,
+    }
+
+    response = requests.get(url, headers=headers, params=params,verify=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        corrected_data = json.dumps(data)
+        return JsonResponse({"message": corrected_data}, status=201)
+    return JsonResponse({"error": "Error in getting identites data"}, status=400)
     
 

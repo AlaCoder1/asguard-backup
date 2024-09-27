174baf95901f16bcee710e5d4fd1db03be2f9405
Ismail <i.bouhlel@numeryx.fr>
2024-09-27 19:53:01 +0100

    ztna models added

--- backend/ztna/models.py
+++ backend/ztna/models.py
@@ -1,3 +1,122 @@
 from django.db import models
+from datetime import datetime
 
 # Create your models here.
+
+class Identities(models.Model):
+    ref_identitie = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    attribute_identitie = models.CharField(max_length=500, null=True, blank=True)
+    type = models.CharField(max_length=200)
+    description = models.CharField(max_length=800,null=True)
+    is_admin = models.BooleanField(default=False)
+    date_creation = models.DateTimeField()
+    date_expiration = models.DateTimeField(null=True, blank=True)
+    token = models.CharField(max_length=1000, unique=True, null=True, blank=True)
+    class Meta:
+        db_table = 'identities'
+    def __str__(self):
+        return self.ref_identitie
+    
+class Enrollements(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    type = models.CharField(max_length=100)
+    identitie = models.ForeignKey(Identities, on_delete=models.CASCADE)
+    class Meta:
+        db_table = 'enrollements'
+
+class InterceptConfigs(models.Model):
+    ref_intercept=models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    protocol = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    low = models.IntegerField()
+    high = models.IntegerField()
+    date_creation = models.DateTimeField()
+    description = models.CharField(max_length=800,null=True)
+    class Meta:
+        db_table = 'interceptconfigs'
+    def __str__(self):
+        return self.ref_intercept
+    
+class HostConfigs(models.Model):
+    ref_host=models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    protocol = models.CharField(max_length=10)
+    address = models.CharField(max_length=100)
+    port = models.IntegerField()
+    date_creation = models.DateTimeField()
+    description = models.CharField(max_length=800,null=True)
+    class Meta:
+        db_table = 'hostconfigs'
+    def __str__(self):
+        return self.ref_host
+    
+class Services(models.Model):
+    ref_service = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    attribute_service = models.CharField(max_length=500, null=True, blank=True)
+    description = models.CharField(max_length=800,null=True)
+    encryption = models.BooleanField(default=False)
+    intercept = models.ForeignKey(InterceptConfigs, on_delete=models.CASCADE)
+    host = models.ForeignKey(HostConfigs, on_delete=models.CASCADE)
+    date_creation = models.DateTimeField()
+    class Meta:
+        db_table = 'servicesztna'
+
+class Relays(models.Model):
+    ref_relay=models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    attribute_relay = models.CharField(max_length=500, null=True, blank=True)
+    tunneler = models.BooleanField(default=False)
+    traversal = models.BooleanField(default=False)
+    online = models.BooleanField(default=False)
+    verified = models.BooleanField(default=False)
+    date_creation = models.DateTimeField()
+    description = models.CharField(max_length=800,null=True)
+    class Meta:
+        db_table = 'relays'
+    def __str__(self):
+        return self.ref_relay
+    
+class RelaysPolicy(models.Model):
+    ref_relay_policy = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    semantique = models.CharField(max_length=10, blank=False)
+    description = models.CharField(max_length=800,null=True)
+    relay = models.ForeignKey(Relays, on_delete=models.CASCADE)
+    identity = models.ForeignKey(Identities, on_delete=models.CASCADE)
+    identity_attribute=models.CharField(max_length=500, null=True, blank=True)
+    relay_attribute=models.CharField(max_length=500, null=True, blank=True)
+
+    date_creation = models.DateTimeField()
+    class Meta:
+        db_table = 'relaypolicy'
+
+class ServicesPolicy(models.Model):
+    ref_service_policy = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    semantique = models.CharField(max_length=10, blank=False)
+    type = models.CharField(max_length=10, blank=False)
+    description = models.CharField(max_length=800,null=True)
+    service_attribute = models.CharField(max_length=10, blank=False)
+    identity_attribute = models.CharField(max_length=10, blank=False)
+    service = models.ForeignKey(Services, on_delete=models.CASCADE)
+    identity = models.ForeignKey(Identities, on_delete=models.CASCADE)
+    date_creation = models.DateTimeField()
+    class Meta:
+        db_table = 'servicepolicies'
+
+class ServicesRelaysPolicy(models.Model):
+    ref_service_relay_policy = models.CharField(max_length=200, unique=True)
+    name = models.CharField(max_length=500, unique=True)
+    semantique = models.CharField(max_length=10, blank=False)
+    description = models.CharField(max_length=800,null=True)
+    service_attribute = models.CharField(max_length=10, blank=False)
+    relay_attribute = models.CharField(max_length=10, blank=False)
+    service = models.ForeignKey(Services, on_delete=models.CASCADE)
+    relay = models.ForeignKey(Relays, on_delete=models.CASCADE)
+    date_creation = models.DateTimeField()
+    class Meta:
+        db_table = 'servicerelaypolicies'
---
+++ backend/ztna/serializers.py
@@ -0,0 +1,110 @@
+from rest_framework import serializers
+from backend.ztna.models import *
+
+class EnrollementsSerializerGet(serializers.ModelSerializer):
+    identitie_id = serializers.PrimaryKeyRelatedField(source='identitie', queryset=Identities.objects.all())
+    class Meta:
+        model = Enrollements
+        fields = ['date','time','type','identitie_id']
+
+class IdentitiesSerializerGet(serializers.ModelSerializer):
+    class Meta:
+        model = Identities
+        fields = "__all__"
+
+class IdentitiesSerializerUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = Identities
+        fields = ['token', 'date_expiration']
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+
+class InterceptConfigsSerializerGet(serializers.ModelSerializer):
+    class Meta:
+        model = InterceptConfigs
+        fields = "__all__"
+
+class HostConfigsSerializerGet(serializers.ModelSerializer):
+    class Meta:
+        model = HostConfigs
+        fields = "__all__"
+
+class ServicesSerializerGet(serializers.ModelSerializer):
+    host_id = serializers.PrimaryKeyRelatedField(source='host', queryset=HostConfigs.objects.all())
+    intercept_id = serializers.PrimaryKeyRelatedField(source='intercept', queryset=InterceptConfigs.objects.all())
+
+    class Meta:
+        model = Services
+        fields = ['ref_service', 'name', 'attribute_service', 'description', 'encryption', 'intercept_id', 'host_id', 'date_creation']
+
+class RelaysSerializerGet(serializers.ModelSerializer):
+    class Meta:
+        model = Relays
+        fields = ['ref_relay','name','attribute_relay','tunneler','traversal','online','verified','date_creation','description']
+
+class RelaySerializerUpdate(serializers.ModelSerializer):
+    class Meta:
+        model = Relays
+        fields = ['online', 'verified']
+
+    def update(self, instance, validated_data):
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+        instance.save()
+        return instance
+    
+class RelaysPolicySerializerGet(serializers.ModelSerializer):
+    relay_id = serializers.PrimaryKeyRelatedField(source='relay', queryset=Relays.objects.all())
+    identity_id = serializers.PrimaryKeyRelatedField(source='identity', queryset=Identities.objects.all())
+    class Meta:
+        model = RelaysPolicy
+        fields = [
+            'ref_relay_policy',
+            'name',
+            'semantique',
+            'description',
+            'relay_id',
+            'identity_id',
+            'identity_attribute',
+            'relay_attribute',
+            'date_creation'
+        ]
+
+class ServicesPolicySerializerGet(serializers.ModelSerializer):
+    service_id = serializers.PrimaryKeyRelatedField(source='service', queryset=Services.objects.all())
+    identity_id = serializers.PrimaryKeyRelatedField(source='identity', queryset=Identities.objects.all())
+    class Meta:
+        model = ServicesPolicy
+        fields = [
+            'ref_service_policy',
+            'name',
+            'semantique',
+            'description',
+            'type',
+            'service_id',
+            'identity_id',
+            'identity_attribute',
+            'service_attribute',
+            'date_creation'
+        ]
+
+class ServicesRelaysPolicySerializerGet(serializers.ModelSerializer):
+    service_id = serializers.PrimaryKeyRelatedField(source='service', queryset=Services.objects.all())
+    relay_id = serializers.PrimaryKeyRelatedField(source='relay', queryset=Relays.objects.all())
+    class Meta:
+        model = ServicesRelaysPolicy
+        fields = [
+            'ref_service_relay_policy',
+            'name',
+            'semantique',
+            'description',
+            'service_id',
+            'relay_id',
+            'relay_attribute',
+            'service_attribute',
+            'date_creation'
+        ]

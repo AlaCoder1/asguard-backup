b1a70b98bc282956b4887d5d708d30d5fb471283
Souhail <s.aloui@numeryx.fr>
2025-01-08 11:08:43 +0100

    add required in module user

--- src/components/modals/Modal.vue
+++ src/components/modals/Modal.vue
@@ -36,7 +36,7 @@
               <v-row>
                 <v-col cols="12" class="mb-n6">
                   <v-text-field
-                    :label="$t('PageGeneral.ServerName')"
+                    :label="`${$t('PageGeneral.ServerName')} *`"
                     v-model="state.name"
                   ></v-text-field>
 
@@ -56,7 +56,7 @@
                 </v-col>
                 <v-col cols="12" class="mb-n6">
                   <v-text-field
-                    :label="$t('PageGeneral.SearchBase')"
+                    :label="`${$t('PageGeneral.SearchBase')} *`"
                     v-model="state.searchBase"
                   ></v-text-field>
 
@@ -77,7 +77,7 @@
                 <v-col cols="12" class="mb-n6">
                   <v-select
                     v-model="state.serverType"
-                    :label="$t('PageGeneral.ServerType')"
+                    :label="`${$t('PageGeneral.ServerType')} *`"
                     item-title="name"
                     item-value="slug"
                     return-object
@@ -126,6 +126,14 @@
           </v-card-text>
 
           <v-card-actions class="mt-3 actionBtnServer">
+            <div class="text-start ml-6 mt-3">
+              <span class="text-sm">
+                <span class="text-red text-lg">*</span>
+                {{ $t("errors.oblig") }}</span
+              >
+            </div>
+            <span></span>
+            <v-spacer></v-spacer>
             <v-btn
               color="indigo-darken-3"
               :rounded="true"

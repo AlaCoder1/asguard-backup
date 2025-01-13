d9b8d2dee9f41638cb3cb8c55d8e6fa571a10429
amanibouhrira <a.bouhrira@numeryx.fr>
2025-01-13 22:55:39 +0100

    documentation and optimisation of managementLogs application

--- backend/managementLogs/views.py
+++ backend/managementLogs/views.py
@@ -13,11 +13,44 @@ import os
 import gzip
 import zipfile
 from io import BytesIO
-# Create your views here.
+@swagger_auto_schema(
+    method='GET',
+    operation_summary="API to retrieve log entry details.",
+    responses={
+        200: openapi.Response(
+            description=(
+                "Details of the log entry retrieved successfully. "
+                "Each log entry contains the following attributes:\n"
+                "- **date**: The date and time of the log event (e.g., 'Dec 11 08:10:35').\n"
+                "- **process**: The process that generated the log entry, including its name and ID (e.g., 'Asguard sudo[1649]').\n"
+                "- **message**: A detailed log message (e.g., 'pam_unix(sudo:session): session opened for user root(uid=0) by (uid=1001)').\n"
+                "- **id**: The unique identifier for the log entry."
+            ),
+
+        ),
+    }
+)
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication])
 def get_logs_data(request):
-    """API to get the last 1000 logs from the database"""
+    """
+    API to get the last 1000 logs from the database.
+
+    This function retrieves the last 1000 log entries from the database and returns them as a JSON response.
+    
+    Parameters:
+    request (HttpRequest): The incoming request object. 
+
+    Returns:
+    JsonResponse: A JSON response containing the log data. The response is formatted as {"data": logs_data},
+    where logs_data is a list of log entries. 
+    Each log entry is represented as a dictionary with the following keys:
+    - date: The date and time of the log event.
+    - process: The process that generated the log entry, including its name and ID.
+    - message: A detailed log message.
+    - id: The unique identifier for the log entry.
+
+    """
     if request.method == 'GET':
         list_logs = []
         logs_object = LogsData.objects.all().order_by('-id')[:1000]
@@ -26,29 +59,66 @@ def get_logs_data(request):
         for log in res:
             log['fields']['id'] = log["pk"]
             list_logs.append(log['fields'])
-        return JsonResponse({"data": list_logs})
+        return JsonResponse({"data": list_logs},status=200)
     
-    
+@swagger_auto_schema(
+    method='GET',
+    operation_summary="API to retrieve and format log data for download.",
+    responses={
+        200: openapi.Response(
+            description=(
+                "Details of the log entry retrieved successfully. "
+                "Each log entry contains the following attributes:\n"
+                "- **date**: The date and time of the log event (e.g., 'Dec 11 08:10:35').\n"
+                "- **process**: The process that generated the log entry, including its name and ID (e.g., 'Asguard sudo[1649]').\n"
+                "- **message**: A detailed log message (e.g., 'pam_unix(sudo:session): session opened for user root(uid=0) by (uid=1001)').\n"
+                "- **id**: The unique identifier for the log entry."
+            ),
+
+        ),
+    }
+)    
     
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication])
 def download_logs_data(request):
-    """API to get data to download it into file"""
+    """
+    API to retrieve and format log data for download.
+
+    This function is responsible for retrieving log data from a system-specific source,
+    formatting it into a JSON response, and returning it as a downloadable file.
+
+    Parameters:
+    request (HttpRequest): The incoming request object. 
+
+    Returns:
+    JsonResponse: A JSON response containing the log data. The response is formatted as {"data": logs_data},
+    where logs_data is a list of log entries. Each log entry is represented as a dictionary with the following keys:
+    - date: The date and time of the log event.
+    - process: The process that generated the log entry, including its name and ID.
+    - message: A detailed log message.
+    - id: The unique identifier for the log entry.
+
+    Note: The actual implementation of retrieving log data from the system-specific source is not included in this docstring.
+    """
     if request.method == 'GET':
-        logs_data=get_logs_sys()
-        return JsonResponse({"data": logs_data})
+        logs_data = get_logs_sys()
+        return JsonResponse({"data": logs_data},status=200)
     
   
     
     
 @swagger_auto_schema(
     method='get',
+    operation_summary="API to retrieve logrotate data based on a specific service.",
     manual_parameters=[
         openapi.Parameter(
             'service',
             openapi.IN_PATH,
             description="The name of the service for which logrotate data is requested",
-            type=openapi.TYPE_STRING
+            type=openapi.TYPE_STRING,
+            enum=['WAF','OpenVPN',"Squid"]
+            
         )
     ],
     responses={200: openapi.Response('Logrotate data retrieved successfully')}
@@ -76,10 +146,28 @@ def get_logrotate_by_service(request, service):
             for log in res:
                 log['fields']['id'] = log["pk"]
                 list_logs.append(log['fields'])
-        return JsonResponse({"data": list_logs})
+        return JsonResponse({"data": list_logs},status=200)
     
     
-    
+
+@swagger_auto_schema(
+    method='get',
+    operation_summary="API to retrieve all logrotate data",
+    responses={
+    200: openapi.Response(
+            description=(
+                "Details of the log files retrieved successfully. "
+                "Each log file contains the following attributes:\n"
+                "- **service**: The service associated with the log file (e.g., 'IDS/IPS').\n"
+                "- **filename**: The name of the log file (e.g., 'suricata.log-2024-09-09-08:52:38.gz').\n"
+                "- **original_path**: The original directory path of the log file (e.g., '/var/log/suricata/suricata.log').\n"
+                "- **backup_path**: The directory path where backup logs are stored (e.g., '/var/log/suricata/backup_logs').\n"
+                "- **date**: The timestamp indicating when the log file was created or modified (e.g., '2024-09-09-08:52:38').\n"
+                "- **id**: The unique identifier of the log entry."
+            ),
+    )
+    }
+)    
     
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication])
@@ -102,8 +190,19 @@ def get_logrotate_data(request):
         for log in res:
             log['fields']['id'] = log["pk"]
             list_logs.append(log['fields'])
-        return JsonResponse({"data": list_logs})   
+        return JsonResponse({"data": list_logs},status=200)   
 
+
+
+@swagger_auto_schema(
+    method='POST',
+    operation_summary="API to convert a .gz file to a .zip file and return it as a download.",
+    responses={
+    200: "File download successfully!",
+    400: "Error in downloading file"
+    }
+)    
+   
 @api_view(['POST'])
 @authentication_classes([SessionAuthentication])
 def download_logrotate_data(request):
@@ -145,7 +244,13 @@ def download_logrotate_data(request):
         return HttpResponse(f"Error: {str(e)}", status=400)     
         
 
-
+@swagger_auto_schema(
+    method='DELETE',
+    responses={200:"File  deleted successfully .",
+               404:"Error: File does not exist ",
+               400: "Error in deleting"},
+    operation_summary="API DELETE logrotate file ",
+)
 @api_view(['DELETE'])
 @authentication_classes([SessionAuthentication])
 def delete_logrotate_file(request, file_id):
@@ -178,11 +283,25 @@ def delete_logrotate_file(request, file_id):
         except Exception as e:
             return JsonResponse({"msg":f"Error: {str(e)}"}, status=400)
 
-
+@swagger_auto_schema(
+    method='get',
+    operation_summary="API to retrieve logs data based on a specific service and download it.",
+    manual_parameters=[
+        openapi.Parameter(
+            'service',
+            openapi.IN_PATH,
+            description="The path of the service for which logs data is requested",
+            type=openapi.TYPE_STRING,
+           example="/var/log/suricata/suricata.log"
+            
+        )
+    ],
+    responses={200: openapi.Response('Logrotate data retrieved successfully')}
+)
 @api_view(['GET'])
 @authentication_classes([SessionAuthentication])
 def download_logs_service(request,file_path):
     """API to get data to download it into file"""
     if request.method == 'GET':
         logs_data=get_logs_service(file_path)
-        return JsonResponse({"data": logs_data})
+        return JsonResponse({"data": logs_data},status=200)

ae35781b52de170eec6d784bfd83d26fc41467e5
amanibouhrira <a.bouhrira@numeryx.fr>
2025-03-17 15:50:30 +0100

    add command to save logs in database

--- backend/rules/models.py
+++ backend/rules/models.py
@@ -19,5 +19,31 @@ class Rule(models.Model):
         db_table = 'rule'
     def __str__(self):
         return self.rule
+    
+   
+class FirewallLog(models.Model):
+    log = models.CharField(max_length=10000, null=True, unique=True)
+    timestamp=models.CharField(max_length=200, null=True, unique=False)
+    rule=models.CharField(max_length=10000, null=True, unique=False)
+    interfaces=models.CharField(max_length=200, null=True, unique=False)
+    out_mac=models.CharField(max_length=200, null=True, unique=False)
+    src_ip=models.CharField(max_length=200, null=True, unique=False)
+    dst_ip=models.CharField(max_length=200, null=True, unique=False)
+    len_trame=models.CharField(max_length=200, null=True, unique=False)
+    TOS=models.CharField(max_length=200, null=True, unique=False)
+    PREC=models.CharField(max_length=200, null=True, unique=False)
+    TTL=models.CharField(max_length=200, null=True, unique=False)
+    ID=models.CharField(max_length=200, null=True, unique=False)
+    DF=models.CharField(max_length=200, null=True, unique=False)
+    protocole=models.CharField(max_length=200, null=True, unique=False)
+    src_port=models.CharField(max_length=200, null=True, unique=False)
+    dst_port=models.CharField(max_length=200, null=True, unique=False)
+    window_size=models.CharField(max_length=200, null=True, unique=False)
+    RES=models.CharField(max_length=200, null=True, unique=False)
+    ACK=models.CharField(max_length=200, null=True, unique=False)
+    URGP=models.CharField(max_length=200, null=True, unique=False)
+
+    class Meta:
+        db_table = 'firewall_log'
    
     

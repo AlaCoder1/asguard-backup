d12846eb9ef2b4eb681a504dc5baa1f79c16abda
Akram baklouti <a.baklouti@numeryx.fr>
2025-05-21 17:07:22 +0100

    fix dnat and routing modals front

--- src/components/modals/RoutingModal.vue
+++ src/components/modals/RoutingModal.vue
@@ -469,14 +469,18 @@ export default {
       return t("champs.champNumberMax");
     });
 
+     const formatMustBeLikeAdresse = computed(() => {
+      return t("errors.formatMustBeLikeAdresse");
+    });
+
     const rules = computed(() => {
       return {
         network: {
           required: helpers.withMessage(error, required),
           isValidlNetwork: helpers.withMessage(
-            addressForma,
+            formatMustBeLikeAdresse,
             helpers.regex(
-              /^(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.(25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/
+              /^((?!0\.0\.0\.0)(\b(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\.){3}(25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9]?[0-9])\/([1-9]|[12][0-9]|3[0-2]))$/
             )
           ),
         },

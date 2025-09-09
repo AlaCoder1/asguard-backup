7377ef95ffae0bd2890b9788024bcc5439a77556
A.S <s.aloui>
2025-09-09 14:18:44 +0100

    evol indication ztna

--- src/components/modals/ModalAddIntercept.vue
+++ src/components/modals/ModalAddIntercept.vue
@@ -74,7 +74,7 @@
                   <v-text-field
                     id="adress"
                     v-model="adress"
-                    :placeholder="$t('ztna.address')"
+                    :placeholder="`${$t('ztna.address')}*`"
                     :rules="rulesaddress"
                     persistent-placeholder
                   />

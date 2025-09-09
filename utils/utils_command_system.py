123e1b6e75cc2d007b3097f552d62a580eb4c7b5
a.bouhrira <a.bouhrira@numeryx.fr>
2025-09-09 11:52:14 +0100

    add uvicorn restart

--- utils/utils_command_system.py
+++ utils/utils_command_system.py
@@ -3,3 +3,5 @@ from utils.commands_utils import execute_command_without_arguments
 
 def restart_nginx_in_system():
     execute_command_without_arguments(["sudo", "systemctl", "restart", "nginx"])
+def restart_uvicorn_in_system():
+    execute_command_without_arguments(["sudo", "systemctl", "restart", "uvicorn"])
